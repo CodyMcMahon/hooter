e9cd4b46e90c3ebfd2ce1bb8ded7d371b0b02f93
Cody <codyjamesmcmahon@gmail.com>
2016-12-03 09:52:49 +0000

    did a lot of stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   
   devise_for :users
+  resources :posts
   root 'pages#index'
 
   get '/home' => 'pages#home'
 
-  get '/profile' => 'pages#profile'
-
   get '/explore' => 'pages#explore'
+  
+  get '/hoots' => 'pages#hoots'
+  
+  get '/:id' => 'pages#profile'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
